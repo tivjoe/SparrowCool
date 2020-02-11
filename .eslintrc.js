74445968065f0c3525e93ad11eb587619bf6cec3
noobjoe <hackerjoe@163.com>
2020-02-11 17:07:30 +0800

    依赖

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,13 @@
 module.exports = {
   root: true,
   extends: '@react-native-community',
+  "plugins": [
+    // ...
+    "react-hooks"
+  ],
+  "rules": {
+    // ...
+    "react-hooks/rules-of-hooks": "error", // Checks rules of Hooks
+    "react-hooks/exhaustive-deps": "warn" // Checks effect dependencies
+  }
 };
