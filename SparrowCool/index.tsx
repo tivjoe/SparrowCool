0df87653919e72cb8981ec825b0e012f7e4ed577
noobjoe <hackerjoe@163.com>
2020-03-05 21:39:16 +0800

    变成第三方库的形式

--- SparrowCool/index.tsx
+++
@@ -1,3 +0,0 @@
-export { NavigationBar } from './NavigationBar/NavigationBar';
-export { NavigationBackBar } from './NavigationBar/NavigationBackBar';
-export { NavigationSpringBar } from './NavigationBar/NavigationSpringBar/index';
---
+++ index.ts
@@ -0,0 +1,3 @@
+export { NavigationBar } from './SparrowCool/NavigationBar/NavigationBar';
+export { NavigationBackBar } from './SparrowCool//NavigationBar/NavigationBackBar';
+export { NavigationSpringBar } from './SparrowCool//NavigationBar/NavigationSpringBar/index';
