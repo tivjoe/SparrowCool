f047101c35963185d1dadf9479fbb7d679530cdb
noobjoe <hackerjoe@163.com>
2020-10-24 21:52:06 +0800

    <Switch /> add params

--- sparrowcool/Switch/index.tsx
+++ sparrowcool/Switch/index.tsx
@@ -5,50 +5,60 @@ import { Animated, StyleSheet, TouchableWithoutFeedback, View } from 'react-nati
  * @file 开关选择组件
  */
 interface Props {
-    value: boolean
-    onValueChange: () => void
-    disabled?: boolean
+    value: boolean // 表示此开关是否打开。默认为 false（关闭状态）。
+    onValueChange: () => void // 当值改变的时候调用此回调函数，参数为新的值。
+    disabled?: boolean // 如果为true则禁用此组件的交互。
+    onOpenBgColor?: string // 打开时的背景色
+    onCloseBgColor?: string // 关闭时的背景色
+    circleColor?: string // 圆的颜色
+    scalingRatio?: number // 是否需要放大或者缩小
 };
 
-export const Switch: React.FC<Props> = React.memo(({ value, onValueChange, disabled }) => {
-
-    const [transX] = React.useState(new Animated.Value(value === true ? 18 : 0));
+export const Switch: React.FC<Props> = React.memo(({
+    value,
+    onValueChange,
+    disabled,
+    onOpenBgColor = '#65C066',
+    onCloseBgColor = '#dddddd',
+    circleColor = 'white',
+    scalingRatio = 1
+}) => {
+    const [transX] = React.useState(new Animated.Value(value === true ? 18 * scalingRatio : 0));
 
     React.useEffect(() => {
         Animated.spring(transX, {
-            toValue: value === true ? 18 : 0,
-            speed: 15,
-            bounciness: 12,
-            restDisplacementThreshold: 10,
-            restSpeedThreshold: 10,
+            toValue: value === true ? 18 * scalingRatio : 0,
+            speed: 15 * scalingRatio,
+            bounciness: 12 * scalingRatio,
+            restDisplacementThreshold: 10 * scalingRatio,
+            restSpeedThreshold: 10 * scalingRatio,
             useNativeDriver: true,
         }).start()
     }, [value]);
 
     return (
         <TouchableWithoutFeedback disabled={disabled} onPress={onValueChange} >
-            <View style={[styles.container, value === true ? styles.onOpenContainer : styles.onCloseContainer]}>
-                <Animated.View style={[styles.circle, { transform: [{ translateX: transX }], }]} />
+            <View style={[
+                styles.container,
+                { width: 48 * scalingRatio, height: 27 * scalingRatio },
+                { backgroundColor: value === true ? onOpenBgColor : onCloseBgColor }
+            ]}>
+                <Animated.View style={[
+                    styles.circle,
+                    { backgroundColor: circleColor },
+                    { transform: [{ translateX: transX }], }]}
+                />
             </View>
-        </TouchableWithoutFeedback>
+        </TouchableWithoutFeedback >
     );
-
 });
 
 
 const styles = StyleSheet.create({
     container: {
-        width: 48,
-        height: 27,
         borderRadius: 25,
         justifyContent: 'center',
         paddingHorizontal: 3
-    },
-    onOpenContainer: {
-        backgroundColor: '#65C066'
-    },
-    onCloseContainer: {
-        backgroundColor: '#dddddd'
     },
     circle: {
         width: 25,
