25b523d388c6f67def90c04789ed9759b8275509
noobjoe <hackerjoe@163.com>
2020-10-24 21:09:24 +0800

    add component

--- index.ts
+++ index.ts
@@ -1,2 +1,3 @@
 export { NavigationBar } from './sparrowcool/NavigationBar'; // 导航栏
 export { ScrollVaryNavbar } from './sparrowcool/ScrollVaryNavbar'; // 滚动时动态切换导航栏
+export { Switch } from './SparrowCool/Switch'; // 开关选择组件
