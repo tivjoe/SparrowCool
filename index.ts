880d6f1c9ecfcc48c43d872816fff987b334c3b6
HackJoe <hackerjoe@163.com>
2020-10-29 17:16:09 +0800

    directory rename with lib

--- index.ts
+++ index.ts
@@ -1,9 +1,9 @@
-export { NavigationBar } from './sparrowcool/NavigationBar'; // 导航栏
-export { ScrollVaryNavbar } from './sparrowcool/ScrollVaryNavbar'; // 滚动时动态切换导航栏
-export { Switch } from './sparrowcool/Switch'; // 开关选择组件
-export { ImmersiveSafeAreaView } from './sparrowcool/ImmersiveSafeAreaView'; // 沉浸式安全距离组件
-export { ImmersiveStatusBar } from './sparrowcool/ImmersiveStatusBar'; // 动态更改状态栏文本hook
-export { utilLoading } from './sparrowcool/Loading'; // loading组件
-export type { LoadingComponentProps } from './sparrowcool/Loading/Loading';
-export { utilAlert } from './sparrowcool/Alert'; // alert组件
-export type { AlertComponentProps } from './sparrowcool/Alert/Alert';
+export { NavigationBar } from './lib/NavigationBar'; // 导航栏
+export { ScrollVaryNavbar } from './lib/ScrollVaryNavbar'; // 滚动时动态切换导航栏
+export { Switch } from './lib/Switch'; // 开关选择组件
+export { ImmersiveSafeAreaView } from './lib/ImmersiveSafeAreaView'; // 沉浸式安全距离组件
+export { ImmersiveStatusBar } from './lib/ImmersiveStatusBar'; // 动态更改状态栏文本hook
+export { utilLoading } from './lib/Loading'; // loading控制
+export type { LoadingComponentProps } from './lib/Loading/Loading'; // loading组件Props
+export { utilAlert } from './lib/Alert'; // alert控制
+export type { AlertComponentProps } from './lib/Alert/Alert'; // alert组件Props
